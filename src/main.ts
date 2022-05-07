ef6cf1f231ce6e0a0213289ecff786af47eb7023
Ryo Nakamura <r7kamura@gmail.com>
2022-05-07 11:49:05 +0900

    Prefer execCommand to Clipboard API because it has a bug...
    - https://stackoverflow.com/questions/62270880/document-not-focused-error-when-writing-to-clipboard-using-clipboard-api-in-chro
    - https://bugs.chromium.org/p/chromium/issues/detail?id=874848

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import "./style.css";
 
+function copy({ html, plain }: { html: string; plain: string }) {
+  const listener = (event: any) => {
+    event.clipboardData.setData("text/html", html);
+    event.clipboardData.setData("text/plain", plain);
+    event.preventDefault();
+  };
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+}
+
 chrome.tabs.query(
   {
     active: true,
@@ -7,14 +18,9 @@ chrome.tabs.query(
   },
   (tabs) => {
     const { title, url } = tabs[0];
-    const htmlLink = `<a href="${url}">${title}</a>`;
-    const plainLink = `"${title}" ${url}`;
-
-    navigator.clipboard.write([
-      new ClipboardItem({
-        "text/plain": new Blob([plainLink], { type: "text/plain" }),
-        "text/html": new Blob([htmlLink], { type: "text/html" }),
-      }),
-    ]);
+    copy({
+      html: `<a href="${url}">${title}</a>`,
+      plain: `"${title}" ${url}`,
+    });
   }
 );
